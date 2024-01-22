72af7f734e33353ee1c191943216ec0806883dc0
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2024-01-22 09:50:21 -0700

    Add TailwindCSS styles to Root

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,8 +1,8 @@
 export default function Root() {
   return (
     <>
-      <h1>Booksort</h1>
-      <p>Organize your personal library</p>
+      <h1 className='text-4xl font-bold text-center'>Booksort</h1>
+      <p className='text-center'>Organize your personal library</p>
     </>
   );
 }
