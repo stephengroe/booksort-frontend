5100d393a4a5e5a1c29bcf66b85e96128976a299
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2024-01-22 09:47:00 -0700

    Add 'Root' route
    * Added basic 'Root' to React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './routes/root';
 import './index.css';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Test data</div>
+    element: <Root />,
   },
 ]);
 
---
+++ src/routes/root.tsx
@@ -0,0 +1,8 @@
+export default function Root() {
+  return (
+    <>
+      <h1>Booksort</h1>
+      <p>Organize your personal library</p>
+    </>
+  );
+}
