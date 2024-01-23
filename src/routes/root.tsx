b9c9ca53ab45d9b51c292230942f17d1134d7ed6
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2024-01-23 10:11:55 -0700

    Add sidebar and main navigation
    * Added root element for sidebar
    * Included dummy navigation (doesn't use dynamic categories)

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -3,6 +3,58 @@ export default function Root() {
     <>
       <h1 className='text-4xl font-bold text-center'>Booksort</h1>
       <p className='text-center'>Organize your personal library</p>
+
+      <div id="sidebar">
+        <div>
+          <form id="search-form" role="search">
+            <input
+              id="q"
+              aria-label="Search contacts"
+              placeholder="Search"
+              type="search"
+              name="q"
+            />
+            <div
+              id="search-spinner"
+              aria-hidden
+              hidden={true}
+            />
+            <div
+              className="sr-only"
+              aria-live="polite"
+            ></div>
+          </form>
+          <form method="post">
+            <button type="submit">New</button>
+          </form>
+        </div>
+
+        <nav>
+          <h3>Your shelves</h3>
+          <ul>
+            <li>
+              <a href={`/shelf/1`}>Biography</a>
+            </li>
+            <li>
+              <a href={`/shelf/2`}>History</a>
+            </li>
+            <li>
+              <a href={`/shelf/3`}>Technology</a>
+            </li>
+          </ul>
+          <h3>Find by</h3>
+          <ul>
+            <li>
+              <a href={`/authors/all`}>Authors</a>
+            </li>
+            <li>
+              <a href={`/genres/all`}>Genres</a>
+            </li>
+          </ul>
+        </nav>
+
+      </div>
+      <div id="detail"></div>
     </>
   );
 }
