ed3b540c6a875dff2deae9cc488f8285c5893bd2
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2024-02-01 09:50:51 -0700

    Improve mock DB and display
    * Corrected image for Deep Work
    * Changed image URL to medium size
    * Added max height and width for images
    * Changed some shadow effects on BookCard component

--- src/components/book-card.tsx
+++ src/components/book-card.tsx
@@ -6,8 +6,8 @@ interface BookCardProps {
 
 const BookCard: React.FC<BookCardProps> = ({ bookInfo }) => {
   return (
-    <Link to={`/book/${bookInfo.id}`} className='bg-white box-border shadow-sm p-6 mb-4 flex gap-4 items-center w-full rounded-lg hover:shadow-md'>
-      <img src={bookInfo.imageUrl} />
+    <Link to={`/book/${bookInfo.id}`} className='bg-white box-border shadow-sm p-6 mb-4 flex gap-4 items-center w-full rounded-lg transition-all hover:shadow-md'>
+      <img src={bookInfo.imageUrl} className='max-w-16 max-h-24 shadow-lg' />
 
       <div>
         <h3>{bookInfo.title}</h3>
