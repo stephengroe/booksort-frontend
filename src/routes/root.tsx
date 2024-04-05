095623ce1575959ff87bc8049b34dc483b0531b9
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2024-04-04 19:48:28 -0700

    Update Prettier settings

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,5 +1,5 @@
-import { Outlet } from "react-router-dom";
-import NavItem from "../components/nav-item";
+import { Outlet } from 'react-router-dom';
+import NavItem from '../components/nav-item';
 
 export default function Root() {
   type MenuList = {
@@ -8,86 +8,86 @@ export default function Root() {
     link: string;
   };
 
-  const sortByList: Array<MenuList> = [
-    {
-      name: "Authors",
-      quantity: 9,
-      link: "authors/all",
-    },
-    {
-      name: "Genres",
-      quantity: 3,
-      link: "genres/all",
-    },
-    {
-      name: "Languages",
-      quantity: 2,
-      link: "languages/all",
-    },
-  ];
+  const sortByList: Array<MenuList> =
+    [
+      {
+        name: 'Authors',
+        quantity: 9,
+        link: 'authors/all',
+      },
+      {
+        name: 'Genres',
+        quantity: 3,
+        link: 'genres/all',
+      },
+      {
+        name: 'Languages',
+        quantity: 2,
+        link: 'languages/all',
+      },
+    ];
 
-  const shelvesList: Array<MenuList> = [
-    {
-      name: "Biography",
-      quantity: 12,
-      link: "shelf/1",
-    },
-    {
-      name: "Classics",
-      quantity: 7,
-      link: "shelf/2",
-    },
-    {
-      name: "History",
-      quantity: 29,
-      link: "shelf/3",
-    },
-    {
-      name: "Psychology",
-      quantity: 25,
-      link: "shelf/4",
-    },
-    {
-      name: "Technology",
-      quantity: 295,
-      link: "shelf/5",
-    },
-  ];
+  const shelvesList: Array<MenuList> =
+    [
+      {
+        name: 'Biography',
+        quantity: 12,
+        link: 'shelf/1',
+      },
+      {
+        name: 'Classics',
+        quantity: 7,
+        link: 'shelf/2',
+      },
+      {
+        name: 'History',
+        quantity: 29,
+        link: 'shelf/3',
+      },
+      {
+        name: 'Psychology',
+        quantity: 25,
+        link: 'shelf/4',
+      },
+      {
+        name: 'Technology',
+        quantity: 295,
+        link: 'shelf/5',
+      },
+    ];
 
   return (
     <>
       <div
         id="top-bar"
-        className="w-full sticky z-10 top-0 shadow-sm bg-white box-border flex items-center justify-start py-2 border-b border-b-gray-6"
-      >
+        className="w-full sticky z-10 top-0 shadow-sm bg-white box-border flex items-center justify-start py-2 border-b border-b-gray-6">
         <div className="flex items-center justify-center box-border text-2xl font-bold w-72">
           <svg
             className="h-5 w-5 text-blue-8 align-middle mr-2"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
-            stroke-width="4"
-          >
+            stroke-width="4">
             <path
               stroke-linecap="square"
               stroke-linejoin="round"
               d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25"
             />
           </svg>
-          <h1>Booksort</h1>
+          <h1>
+            Booksort
+          </h1>
         </div>
         <form
           id="search-form"
           role="search"
-          className="w-4/12 flex items-center justify-self-start pl-6 p-2"
-        >
+          className="w-4/12 flex items-center justify-self-start pl-6 p-2">
           <svg
             className="h-5 w-5 text-gray-8 align-middle mr-2"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
-            stroke-width="2"
-          >
+            stroke-width="2">
             <path
               stroke-linecap="round"
               stroke-linejoin="round"
@@ -102,17 +102,26 @@ export default function Root() {
             name="q"
             className="focus-visible:outline-none w-full"
           />
-          <div id="search-spinner" aria-hidden hidden={true} />
-          <div className="sr-only" aria-live="polite"></div>
+          <div
+            id="search-spinner"
+            aria-hidden
+            hidden={
+              true
+            }
+          />
+          <div
+            className="sr-only"
+            aria-live="polite"></div>
         </form>
-        <div id="notifications" className="justify-self-end ml-auto">
+        <div
+          id="notifications"
+          className="justify-self-end ml-auto">
           <svg
             className="h-5 w-5 text-slate-400 align-middle mr-2"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
-            stroke-width="2"
-          >
+            stroke-width="2">
             <path
               stroke-linecap="round"
               stroke-linejoin="round"
@@ -120,33 +129,36 @@ export default function Root() {
             />
           </svg>
         </div>
-        <p className="text-sm text-center w-auto px-6 p-2">Welcome, reader</p>
+        <p className="text-sm text-center w-auto px-6 p-2">
+          Welcome,
+          reader
+        </p>
       </div>
 
-      <div id="wrapper" className="box-border w-full h-full flex min-h-max">
+      <div
+        id="wrapper"
+        className="box-border w-full h-full flex min-h-max">
         <div
           id="sidebar"
-          className="min-h-full p-6 relative min-w-72 border-r border-r-gray-6 leading-8 box-border"
-        >
+          className="min-h-full p-6 relative min-w-72 border-r border-r-gray-6 leading-8 box-border">
           <form method="post">
             <button
               type="submit"
-              className="btn flex items-center justify-center"
-            >
+              className="btn flex items-center justify-center">
               <svg
                 className="h-5 w-5 text-white align-middle mr-2"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
-                stroke-width="2"
-              >
+                stroke-width="2">
                 <path
                   stroke-linecap="round"
                   stroke-linejoin="round"
                   d="M12 4.5v15m7.5-7.5h-15"
                 />
               </svg>
-              Add Book
+              Add
+              Book
             </button>
           </form>
           <nav className="leading-10">
@@ -156,20 +168,30 @@ export default function Root() {
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
-                stroke-width="2"
-              >
+                stroke-width="2">
                 <path
                   stroke-linecap="round"
                   stroke-linejoin="round"
                   d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25"
                 />
               </svg>
-              <h3 className="mb-0">Sort by</h3>
+              <h3 className="mb-0">
+                Sort
+                by
+              </h3>
             </div>
             <ul className="ml-2">
-              {sortByList.map((item) => {
-                return <NavItem {...item} />;
-              })}
+              {sortByList.map(
+                (
+                  item
+                ) => {
+                  return (
+                    <NavItem
+                      {...item}
+                    />
+                  );
+                }
+              )}
             </ul>
 
             <div className="flex items-center mt-6">
@@ -178,25 +200,37 @@ export default function Root() {
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
-                stroke-width="2"
-              >
+                stroke-width="2">
                 <path
                   stroke-linecap="round"
                   stroke-linejoin="round"
                   d="M16.5 3.75V16.5L12 14.25 7.5 16.5V3.75m9 0H18A2.25 2.25 0 0 1 20.25 6v12A2.25 2.25 0 0 1 18 20.25H6A2.25 2.25 0 0 1 3.75 18V6A2.25 2.25 0 0 1 6 3.75h1.5m9 0h-9"
                 />
               </svg>
-              <h3 className="mb-0">Your shelves</h3>
+              <h3 className="mb-0">
+                Your
+                shelves
+              </h3>
             </div>
             <ul className="ml-2">
-              {shelvesList.map((item) => {
-                return <NavItem {...item} />;
-              })}
+              {shelvesList.map(
+                (
+                  item
+                ) => {
+                  return (
+                    <NavItem
+                      {...item}
+                    />
+                  );
+                }
+              )}
             </ul>
           </nav>
         </div>
 
-        <div id="detail" className="bg-gray-1 p-12 box-border w-10/12">
+        <div
+          id="detail"
+          className="bg-gray-1 p-12 box-border w-10/12">
           <div className="max-w-screen-md">
             <Outlet />
           </div>
